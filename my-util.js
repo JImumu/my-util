1fe2acfc999626f0bed6e1d7ca5877a9ac322db5
JImumu <jimumu8@163.com>
2018-10-31 14:11:11 +0800

    新增riqibuquan函数

--- my-util.js
+++ my-util.js
@@ -325,3 +325,19 @@ const prefixNum = (num, length) => {
     return arr.join('')
   }
 }
+
+riqibuquan = weeksArr => {
+    var start = new Date(weeksArr[0])
+    var base = +new Date(start.getFullYear(), start.getMonth());
+    var end = new Date(weeksArr[weeksArr.length-1])
+    var oneDay = 24 * 3600 * 1000;
+    var days = (+new Date(end.getFullYear(), end.getMonth()+1) - base) / oneDay
+    var date = [];
+
+    for (var i = 1; i < days; i++) {
+        var now = new Date(base);
+        base += oneDay
+        date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
+    }
+    return date
+}
